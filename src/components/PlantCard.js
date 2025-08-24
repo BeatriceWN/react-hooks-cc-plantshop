1bb978d986c11b4680f9fd8cf7812bba86af9e1a
Beatrice Wambui <bettyneema2@gmail.com>
2025-08-24 06:36:02 +0300

    inititial commit of the PlantCard component

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,40 @@
 import React from "react";
+import
 
-function PlantCard() {
+function PlantCard({ plant, toggleSoldOut, updatePrice, deletePlant }) {
+  const [newPrice, setNewPrice] = useState(plant.price);
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
+      <img src={plant.name} alt={"plant name"} />
       <h4>{"plant name"}</h4>
       <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      
+      //toggle stock
+       {plant.soldOut ? (
+        <button onClick={() => toggleSoldOut(plant.id)}>Out of Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button className="primary" onClick={() => toggleSoldOut(plant.id)}>
+          In Stock
+        </button>
       )}
+
+      //update price
+      <div>
+        <input
+          type="number"
+          value={newPrice}
+          onChange={e => setNewPrice(parseFloat(e.target.value))}
+        />
+        <button onClick={() => updatePrice(plant.id, newPrice)}>
+        Update Price
+        </button>
+        </div>
+
+        //delete plant
+        <button onClick={() => deletePlant(plant.id)} style={{ color: "red" }}>
+          Delete
+        </button>
     </li>
   );
 }
