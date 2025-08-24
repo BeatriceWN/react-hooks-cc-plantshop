7bcd29117de56bbef6dca2b3f574ce3ab66367ab
Beatrice Wambui <bettyneema2@gmail.com>
2025-08-24 06:08:11 +0300

    inititial commit of the PlantList components

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,26 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, toggleSoldOut, updatePrice, deletePlant }) {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map(plant => (
+        <PlantCard 
+        key={plant.id}
+        plant={plant}
+        toggleSoldOut={toggleSoldOut}
+        updatePrice={updatePrice}
+        deletePlant={deletePlant}
+        />
+      ))}
+      </ul>
   );
 }
 
 export default PlantList;
+
+/* 
+loop through plants array
+render a PlantCard for each one
+pass down the action handlers
+*/
